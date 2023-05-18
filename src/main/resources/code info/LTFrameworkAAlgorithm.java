8eecce438f963678db826b11d73d4fc7d418c22b
AlexandruBirta <alexbirta2011@gmail.com>
2023-05-18 14:24:19 +0300

    LT

---
+++ src/main/resources/code info/LTFrameworkAAlgorithm.java
@@ -0,0 +1,156 @@
+//Here's an implementation of the LT-Framework A (Approach) algorithm using adjacency list-based graphs in Java:
+//
+//        In this implementation, the Node class represents a node in the graph. Each node has an ID, a set of neighboring node IDs, and a cluster assignment represented by an integer value. The buildGraph method creates the graph by adding nodes and their corresponding neighbors.
+//
+//        The ltfA method implements the LT-Framework A algorithm. It starts by initializing the clusters randomly. Then, it iteratively propagates labels (cluster assignments) among the nodes based on the labels of their neighbors until convergence. In each iteration, each node examines the cluster assignments of its neighbors and adopts the most frequent cluster(s) among them, randomly selecting one if there are ties. The algorithm continues until no further changes occur in the cluster assignments.
+//
+//        The initializeClusters method initializes the clusters with random assignments. The getRandomElement method is a helper method to select a random element from a set.
+//
+//        You can modify the buildGraph method to create and connect the nodes based on your specific graph structure. Additionally, you can experiment with different graph structures and the number of clusters to observe the resulting clustering produced by the LT-Framework A algorithm.
+
+import java.util.*;
+
+class Node {
+    int id;
+    Set<Integer> neighbors;
+    int cluster;
+
+    public Node(int id) {
+        this.id = id;
+        this.neighbors = new HashSet<>();
+        this.cluster = -1; // -1 represents unassigned cluster
+    }
+
+    public void addNeighbor(int neighborId) {
+        neighbors.add(neighborId);
+    }
+}
+
+public class LTFrameworkAAlgorithm {
+    public static void main(String[] args) {
+        List<Node> graph = buildGraph();
+
+        int numClusters = 2; // Number of clusters to create
+        List<Set<Integer>> clusters = ltfA(graph, numClusters);
+
+        System.out.println("Clusters:");
+        for (int i = 0; i < clusters.size(); i++) {
+            System.out.println("Cluster " + i + ": " + clusters.get(i));
+        }
+    }
+
+    public static List<Set<Integer>> ltfA(List<Node> graph, int numClusters) {
+        List<Set<Integer>> clusters = initializeClusters(graph.size(), numClusters);
+
+        boolean changed;
+        do {
+            changed = false;
+
+            for (Node node : graph) {
+                Map<Integer, Integer> clusterCounts = new HashMap<>();
+
+                // Count the occurrences of neighbor clusters
+                for (int neighbor : node.neighbors) {
+                    Node neighborNode = graph.get(neighbor);
+                    int neighborCluster = neighborNode.cluster;
+                    clusterCounts.put(neighborCluster, clusterCounts.getOrDefault(neighborCluster, 0) + 1);
+                }
+
+                // Find the most frequent cluster(s) among neighbors
+                Set<Integer> maxClusters = new HashSet<>();
+                int maxCount = 0;
+                for (Map.Entry<Integer, Integer> entry : clusterCounts.entrySet()) {
+                    int count = entry.getValue();
+                    if (count > maxCount) {
+                        maxCount = count;
+                        maxClusters.clear();
+                        maxClusters.add(entry.getKey());
+                    } else if (count == maxCount) {
+                        maxClusters.add(entry.getKey());
+                    }
+                }
+
+                // Update the cluster of the node if necessary
+                if (!maxClusters.contains(node.cluster)) {
+                    int newCluster = getRandomElement(maxClusters);
+                    node.cluster = newCluster;
+                    changed = true;
+                }
+            }
+        } while (changed);
+
+        return clusters;
+    }
+
+    private static List<Set<Integer>> initializeClusters(int numNodes, int numClusters) {
+        List<Set<Integer>> clusters = new ArrayList<>();
+
+        // Initialize empty clusters
+        for (int i = 0; i < numClusters; i++) {
+            clusters.add(new HashSet<>());
+        }
+
+        // Assign initial cluster randomly
+        Random random = new Random();
+        for (int i = 0; i < numNodes; i++) {
+            int cluster = random.nextInt(numClusters);
+            clusters.get(cluster).add(i);
+        }
+
+        return clusters;
+    }
+
+    public static List<Node> buildGraph() {
+        // Build your graph here
+        List<Node> graph = new ArrayList<>();
+
+        // Add nodes and their neighbors (connections)
+        Node node0 = new Node(0);
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+
+        node0.addNeighbor(1);
+        node0.addNeighbor(2);
+        node1.addNeighbor(0);
+        node1.addNeighbor(2);
+        node1.addNeighbor(3);
+        node2.addNeighbor(0);
+        node2.addNeighbor(1);
+        node2.addNeighbor(3);
+        node2.addNeighbor(4);
+        node3.addNeighbor(1);
+        node3.addNeighbor(2);
+        node3.addNeighbor(4);
+        node3.addNeighbor(5);
+        node4.addNeighbor(2);
+        node4.addNeighbor(3);
+        node4.addNeighbor(5);
+        node5.addNeighbor(3);
+        node5.addNeighbor(4);
+
+        graph.add(node0);
+        graph.add(node1);
+        graph.add(node2);
+        graph.add(node3);
+        graph.add(node4);
+        graph.add(node5);
+
+        return graph;
+    }
+
+    private static int getRandomElement(Set<Integer> set) {
+        int randomIndex = new Random().nextInt(set.size());
+        int element = 0;
+        for (int num : set) {
+            if (randomIndex == 0) {
+                element = num;
+                break;
+            }
+            randomIndex--;
+        }
+        return element;
+    }
+}
